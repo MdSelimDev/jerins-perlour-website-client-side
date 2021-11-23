161346a56fe90b3bf6224710e3f2a368e7914967
MDSELIM1 <selimakodo58@gmail.com>
2021-11-23 11:45:36 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter , Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Pages/HomePages/Home/Home";
 import Login from "./Components/Pages/UserRegister/Login/Login";
-
-
+import Footer from "./Components/Shared/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-      </Routes>  
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col lg={3}>
+            <div className="footer-location">
+              <p>
+                <i className="fas fa-map-marker-alt"></i>
+                H#1200,(19th,floor),Road #22,Mirpur
+                Dohs,Mahakhali,Dhaka,Bangladesh.
+              </p>
+            </div>
+          </Col>
+          <Col lg={3}>
+            <div className="footer-link">
+              <h3 className="footer-title">Company</h3>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
